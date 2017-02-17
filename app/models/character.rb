4f7a2068c7dff4301b6e8f2e1d734c7fbba80267
rmody3 <rmody3@gmail.com>
2017-02-17 15:06:54 -0500

    Done.

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,24 @@
 class Character < ActiveRecord::Base
-  
+  belongs_to :actor
+  belongs_to :show
+
+  def say_that_thing_you_say
+     "#{self.name} always says: #{self.catchphrase}"
+  end
+
+  def build_show(hash)
+    show = Show.find_or_create_by(hash)
+    self.show_id=show.id
+    self
+  end
+
+  def build_network(hash)
+    network = Network.find_or_create_by(hash)
+    self.show.network_id = network.id
+  end
+
+
+
+
+
 end
